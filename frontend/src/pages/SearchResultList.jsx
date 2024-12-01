8b5910426b4a5218a8ad1d00295db2a43580754f
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-01 17:46:27 +0600

    1st push from arnob

---
+++ frontend/src/pages/SearchResultList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchResultList = () => {
+  return (
+    <div>
+      searchresult
+    </div>
+  )
+}
+
+export default SearchResultList
---
+++ frontend/src/pages/Tours.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tours = () => {
+  return (
+    <div>
+      toure
+    </div>
+  )
+}
+
+export default Tours
